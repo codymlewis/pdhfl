6b08a2a0ac2e6b6ab701d2f6354ae8a626cd7c4c
Cody Lewis <Cody.Lewis@newcastle.edu.au>
2023-09-29 11:58:14 +1000

    Slight tweak to the experiments

--- performance/experiments.sh
+++ performance/experiments.sh
@@ -10,7 +10,7 @@ for framework in "pdhfl" "heterofl" "fjord" "feddrop" "local" "fedavg"; do
 
         for allocation in ${allocations[@]}; do
             for seed in {1..5}; do
-                python main.py --rounds 300 --steps-per-epoch 1 --dataset $dataset --framework $framework --seed $seed --allocation $allocation
+                python main.py --rounds 500 --steps-per-epoch 1 --dataset $dataset --framework $framework --seed $seed --allocation $allocation
             done
         done
     done
