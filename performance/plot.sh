21e533d66707811aa4c529b685e6ecef7f0652b2
Cody Lewis <hello@codymlewis.com>
2023-10-03 16:30:02 +1100

    Removed a comment

--- performance/plot.sh
+++ performance/plot.sh
@@ -4,4 +4,4 @@ for framework in "heterofl" "fjord" "pdhfl"; do
     python main.py --rounds 500 --steps-per-epoch 1 --dataset mnist --framework $framework --allocation sim --seed 1 --gen-plot-data
 done
 
-# python plot.py
+python plot.py
