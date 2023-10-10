1d5a4d15f3b4f3dc26d420a7d1d47404c5c685a7
Cody Lewis <hello@codymlewis.com>
2023-10-10 15:39:25 +1100

    Added partial client participation experiments

--- performance/experiments.sh
+++ performance/experiments.sh
@@ -19,7 +19,12 @@ for framework in ${frameworks[@]}; do
 
                 if [[ $dataset == "nbaiot" ]]; then
                     python main.py --rounds 50 --clients 90 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128
+                    python main.py --rounds 50 --clients 90 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128 --proportion-clients 0.1
+                elif [[ $dataset == "cifar100" ]]; then
+                    python main.py --rounds 50 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128 --proportion-clients 0.1
                 fi
+
+
             done
         done
     done
