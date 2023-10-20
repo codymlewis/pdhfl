ea452842d72ceaa1e067153f25ab244efdb34bb6
Cody Lewis <hello@codymlewis.com>
2023-10-20 15:40:27 +1100

    Speed up jit compilation of CIFAR-100 experiments

--- performance/experiments.sh
+++ performance/experiments.sh
@@ -12,17 +12,27 @@ for framework in ${frameworks[@]}; do
         else
             allocations=("cyclic" "sim")
         fi
-        [[ $dataset == "nbaiot" ]] && rounds=10 || [[ $dataset == "cifar100" ]] && rounds=100 || rounds=50
+
+        if [[ $dataset == "nbaiot" ]]; then
+            rounds=10
+            batch_size=128
+        elif [[ $dataset == "cifar100" ]]; then
+            rounds=100
+            batch_size=32
+        else
+            rounds=50
+            batch_size=128
+        fi
 
         for allocation in ${allocations[@]}; do
             for seed in {1..5}; do
-                python main.py --rounds $rounds --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128
+                python main.py --rounds $rounds --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size $batch_size
 
                 if [[ $dataset == "nbaiot" ]]; then
-                    python main.py --rounds $rounds --clients 90 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128
-                    python main.py --rounds $rounds --clients 90 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128 --proportion-clients 0.1
+                    python main.py --rounds $rounds --clients 90 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size $batch_size
+                    python main.py --rounds $rounds --clients 90 --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size $batch_size --proportion-clients 0.1
                 elif [[ $dataset == "cifar100" ]]; then
-                    python main.py --rounds $rounds --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size 128 --proportion-clients 0.1
+                    python main.py --rounds $rounds --dataset $dataset --framework $framework --seed $seed --allocation $allocation --batch-size $batch_size --proportion-clients 0.1
                 fi
             done
         done
